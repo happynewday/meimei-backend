cd21b537c126b46e8f0987d21f961f25c5ea0a1b
xujian <xujian-129@163.com>
2019-10-04 15:39:31 +0800

    give a default value to uuid

--- src/main/java/com/mm/backend/service/imp/UserBackendServiceImpl.java
+++ src/main/java/com/mm/backend/service/imp/UserBackendServiceImpl.java
@@ -30,6 +30,10 @@ public class UserBackendServiceImpl implements UserBackendService {
             throw new Exception("该账号已存在");
         }
 
+        if(StringUtils.isBlank(uuid)){
+            uuid = "";
+        }
+
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         String encodedPassword =  passwordEncoder.encode(password);
         //String encodedPassword = password;
