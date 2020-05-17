f577489fd1f8fbc35a243b0b199bc0e4753b5839
徐坚 <xujian03@corp.netease.com>
2020-05-17 16:13:09 +0800

    add regist log

--- src/main/java/com/mm/backend/controller/UserBackendController.java
+++ src/main/java/com/mm/backend/controller/UserBackendController.java
@@ -24,6 +24,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import io.swagger.annotations.Api;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import static com.mm.backend.common.ResponseCode.USER_LOGIN_USER_NOT_LOGIN;
@@ -50,9 +51,10 @@ public class UserBackendController {
     @RequestMapping(value = "/regist",method = RequestMethod.POST,
             headers="Content-Type=application/json;charset=UTF-8", produces="application/json;charset=UTF-8")
     @ApiOperation(value = "注册", notes = "注册")
-    RestResult<UserBackendVo> registUser(@RequestBody @Validated RegistUserBackendAction action, HttpServletResponse response) {
+    RestResult<UserBackendVo> registUser(@RequestBody @Validated RegistUserBackendAction action, HttpServletResponse response, HttpServletRequest request) {
         try {
             UserBackendVo userInfo = userBackendService.userRegist(action.getUuid(), action.getUsername(), action.getPassword());
+            logger.warn("user regist, username={}, uuid={}, remoteAddr={}", action.getUsername(), action.getUuid(), request.getRemoteAddr());
             userInfo.setValidTime("2020-11-10");
             response.setHeader("x-auth-token", userInfo.getAccess_token());
             return RestResult.createBySuccess(userInfo);
@@ -64,11 +66,11 @@ public class UserBackendController {
     @RequestMapping(value = "/login",method = RequestMethod.POST,
             headers="Content-Type=application/json;charset=UTF-8", produces="application/json;charset=UTF-8")
     @ApiOperation(value = "登录", notes = "登录")
-    RestResult<UserBackendVo> login(@RequestBody @Validated LoginBackendAction action, HttpServletResponse response) {
+    RestResult<UserBackendVo> login(@RequestBody @Validated LoginBackendAction action, HttpServletResponse response, HttpServletRequest request) {
         try {
             UserBackendVo userInfo = userBackendService.login(action.getUsername(), action.getPassword(), action.getUuid());
             response.setHeader("x-auth-token", userInfo.getAccess_token());
-            logger.warn("user login, username={}, uid={}", action.getUsername(), userInfo.getUserId());
+            logger.warn("user login, username={}, uid={}, remoteAddr={}", action.getUsername(), userInfo.getUserId(), request.getRemoteAddr());
             userInfo.setValidTime("2020-11-10");
             return RestResult.createBySuccess(userInfo);
         } catch (BusinessException e){
